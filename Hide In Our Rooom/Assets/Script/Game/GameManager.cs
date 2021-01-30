cbc436ca7cdcd27acd59d0703ddf0418f7c6ccb9
Bryan Bachelet <bryanbacheletpro@gmail.com>
2021-01-30 11:04:08 +0100

    Photon + Test sprite Iso

---
+++ Hide In Our Rooom/Assets/Script/Game/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace GuerhoubaGame
+{
+
+
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        public static GameManager gameManager;
+
+        [Tooltip("The prefab to use  for representing the player")]
+        public GameObject playerPrefab;
+
+
+
+        private void Start()
+        {
+            gameManager = this;
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            }
+            if (PlayerManager.LocalPlayerInstance == null)
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManager.GetActiveScene().name);
+
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
+            }
+            else
+            {
+                Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+            }
+        }
+
+        #region Photon Callbacks
+
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRooom() {0}", other.NickName);
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", otherPlayer.NickName);
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+                LoadArena();
+            }
+        }
+
+
+        #endregion
+
+        #region Public Methods();
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+        #endregion
+
+    }
+}
---
+++ Hide In Our Rooom/Assets/Script/Game/PlayerManager.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+using Photon.Pun;
+using Photon.Pun.Demo.PunBasics;
+using Photon.Realtime;
+using System;
+
+namespace GuerhoubaGame
+{
+
+    public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
+    {
+        #region Private Fields
+        
+        bool isFiring;
+
+        #endregion
+
+
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
+
+
+        #region IPunObservable implementation
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                stream.SendNext(isFiring);
+                stream.SendNext(Health);
+            }
+            else
+            {
+                this.isFiring = (bool)stream.ReceiveNext();
+                this.Health = (float)stream.ReceiveNext();
+            }
+        }
+
+        #endregion 
+
+        #region Public Fields
+
+        [Tooltip("The current Health of our Player")]
+        public float Health = 1f;
+
+        #endregion
+
+
+        #region MonoBehaviour Callbacks
+
+        private void Awake()
+        {
+            if (photonView.IsMine)
+            {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+            DontDestroyOnLoad(this.gameObject);
+
+           
+        }
+
+        private void Start()
+        {
+           
+#if UNITY_5_4_OR_NEWER
+            // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+#endif
+
+        }
+
+        // Update is called once per frame
+        void Update()
+
+        {
+            if (photonView.IsMine)
+            {
+
+                ProcessInputs();
+                if (Health <= 0f)
+                {
+                    GameManager.gameManager.LeaveRoom();
+                }
+
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+            if (!other.name.Contains("Beam"))
+            {
+                return;
+            }
+            Health -= 0.1f;
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+            if (!other.name.Contains("Beam"))
+            {
+                return;
+            }
+            Health -= 0.1f * Time.deltaTime;
+        }
+
+#if !UNITY_5_4_OR_NEWER
+    // <summary>See CalledOnLevelWasLoaded. Outdated in Unity 5.4.</summary>
+    
+        void OnLevelWasLoaded(int level)
+    {
+    this.CalledOnLevelWasLoaded(level);
+    }
+
+#endif
+
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+        }
+
+        public override void OnDisable()
+        {
+            // Always call the base to remove callbacks
+            base.OnDisable();
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        #endregion
+
+        #region Custom
+
+#if UNITY_5_4_OR_NEWER
+
+        void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+        {
+            this.CalledOnLevelWasLoaded(scene.buildIndex);
+        }
+
+#endif
+
+        private void ProcessInputs()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (!isFiring)
+                {
+                    isFiring = true;
+                }
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                if (isFiring)
+                {
+                    isFiring = false;
+                }
+            }
+        }
+        #endregion
+    }
+}
